e857993421b0221caf632d32f8de637ed2cde8d4
Karina <aiwpront@gmail.com>
2022-02-08 21:07:23 +0400

    added model

---
+++ src/test/java/com/github/jjfhj/models/BookList.java
@@ -0,0 +1,91 @@
+package com.github.jjfhj.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BookList {
+
+    private String isbn;
+    private String title;
+    private String subTitle;
+    private String author;
+    @JsonProperty("publish_date")
+    private String publishDate;
+    private String publisher;
+    private String pages;
+    private String description;
+    private String website;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubTitle() {
+        return subTitle;
+    }
+
+    public void setSubTitle(String subTitle) {
+        this.subTitle = subTitle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+}
--- src/test/java/com/github/jjfhj/models/BookListData.java
+++ src/test/java/com/github/jjfhj/models/BookListData.java
@@ -1,13 +1,19 @@
-package com.github.jjfhj.lombok;
+package com.github.jjfhj.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
 
-@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BookListData {
 
     @JsonProperty("books")
     private BookList[] books;
+
+    public BookList[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(BookList[] books) {
+        this.books = books;
+    }
 }
