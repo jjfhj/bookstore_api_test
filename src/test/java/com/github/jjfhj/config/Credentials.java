5e81970eb98f6e932138541f01af97e907de2974
Karina <aiwpront@gmail.com>
2022-02-08 21:07:01 +0400

    added credentials

--- src/test/java/com/github/jjfhj/config/Credentials.java
+++ src/test/java/com/github/jjfhj/config/Credentials.java
@@ -3,5 +3,6 @@ package com.github.jjfhj.config;
 import org.aeonbits.owner.ConfigFactory;
 
 public class Credentials {
-    public static final CredentialsConfig CREDENTIALS_CONFIG = ConfigFactory.create(CredentialsConfig.class, System.getProperties());
+    public static final CredentialsConfig CREDENTIALS_CONFIG =
+            ConfigFactory.create(CredentialsConfig.class, System.getProperties());
 }
